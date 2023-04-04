eae6e092a8db3468897526d997af5ff4d4ae1d09
rosahoffer <rosahoffer@live.nl>
2023-04-04 11:52:03 +0200

    try

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 
 import express from 'express'
-import { fetchJson, postJson} from '../helpers/fetchWrapper.js'
+import { fetchJson, postJson } from '../helpers/fetchWrapper.js'
 
 dotenv.config()
 
@@ -14,42 +14,42 @@ index.use(express.urlencoded({ extended: true }))
 // Route voor de index
 index.get('/', async function (req, res) {
   const url = process.env.API_URL + '/stekjes?first=3'
-  fetchJson(url).then ((data)=> {
-  res.render('index', data)
-})
+  fetchJson(url).then((data) => {
+    res.render('index', data)
+  })
 })
 
 // Route voor index 2
 index.get('/index2', async function (req, res) {
   const url = process.env.API_URL + '/stekjes'
-  fetchJson(url).then ((data)=> {
+  fetchJson(url).then((data) => {
     res.render('index2', data)
-})
+  })
 })
 
 // Route voor stekje aanmelden form
 index.get('/newStekje', async function (req, res) {
   const url = process.env.API_URL
-  fetchJson(url).then ((data)=> {
+  fetchJson(url).then((data) => {
     res.render('stekjeaanmelden', data)
+  })
 })
-})
- 
+
 // Route voor stekje posten
 index.post('/newStekje', async function (req, res) {
   const url = process.env.API_URL + '/stekjes'
 
   console.log(req.body)
 
-  postJson(url,req.body).then ((data)=> {
-    let newStekje = { ... req.body }
+  postJson(url, req.body).then((data) => {
+    let newStekje = { ...req.body }
 
-    if(data.success){
+    if (data.success) {
       res.redirect('/')
-    }else{
+    } else {
       console.log('Er gaat wat mis')
     }
-})
+  })
 })
 
 export default index
